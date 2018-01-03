5548769b74026d73de0857d081aaa5ebefa5c52d
Autumn Jung <autumnkangas@gmail.com>
2018-01-03 16:08:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,12 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
-  end
   elsif int % 3 == 0
     puts "Fizz"
-  end
   elsif int % 5 == 0
     puts "Buzz"
-  end
   else
     nil
+  end
   end
 end
